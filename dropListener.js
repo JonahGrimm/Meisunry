09f89ad6a25833a61eeef3309c5656f796483edd
JonahGrimm <jonahmichaelgrimm@gmail.com>
2023-08-27 23:20:54 -0700

    Fix bug of drop zone area not working when selected folder has no images. Fix drop event listener working inconsistently

--- dropListener.js
+++ dropListener.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dropZone = document.getElementById('grid-wrapper');
+  const dropZone = document.getElementById('drop-zone');
 
   dropZone.addEventListener('dragover', (event) => {
     event.preventDefault();
   });
 
   dropZone.addEventListener('drop', (event) => {
-    event.preventDefault();
-
     const files = event.dataTransfer.files;
     // Iterate through the items to find folders
     for (let i = 0; i < files.length; i++) {
